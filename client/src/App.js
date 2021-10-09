16f91efd7b537304fe006f3a8b679cd84faf1fcf
Janice Mathias <melonijanice@gmail.com>
2021-10-09 15:18:47 -0500

    Deployment

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Router> 
-        <Main path="/product/"/>
+        <Main path="/" default/>
         <Detail path="/product/:id" />
         <Edit path="/product/:id/edit" />
       </Router>
